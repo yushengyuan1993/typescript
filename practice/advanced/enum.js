20bbcc4c071bc41e60bb4cc85a3faa0e79fab725
yushare <582718643@qq.com>
2019-11-04 10:04:22 +0800

    update

--- practice/advanced/enum.js
+++ practice/advanced/enum.js
@@ -14,14 +14,14 @@ var Weekly;
     Weekly[Weekly["Fri"] = 5] = "Fri";
     Weekly[Weekly["Sat"] = 6] = "Sat";
 })(Weekly || (Weekly = {}));
-;
 console.log(Weekly[0]); // Sun
 console.log(Weekly[3]); // Wen
 console.log(Weekly[6]); // Sat
 console.log(Weekly['Sun']); // 0
 console.log(Weekly['Wen']); // 3
 console.log(Weekly['Sat']); // 6
-console.log(Weekly); // 
+console.log(Weekly); // {"0":"Sun","1":"Mon","2":"Tue","3":"Wen","4":"Thu","5":"Fri","6":"Sat","Sun":0,"Mon":1,"Tue":2,"Wen":3,"Thu":4,"Fri":5,"Sat":6}
+console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
 // 2. 手动赋值
 var Color;
 (function (Color) {
@@ -29,7 +29,6 @@ var Color;
     Color[Color["Green"] = 4] = "Green";
     Color[Color["Blue"] = 5] = "Blue";
 })(Color || (Color = {}));
-;
 console.log(Color['Red']); // 3
 console.log(Color['Green']); // 4
 console.log(Color['Blue']); // 5
@@ -38,3 +37,18 @@ console.log(Color[3]); // Red
 console.log(Color[4]); // Green
 console.log(Color[5]); // Blue
 console.log(Color); // { '3': 'Red', '4': 'Green', '5': 'Blue', Red: 3, Green: 4, Blue: 5 }
+console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
+// 3. 只可以被覆盖，但是最好不要出现覆盖的情况
+// 4. 手动赋值的 枚举项 可以不是数字，此时需要使用类型断言来让 tsc 无视类型检查（编译出的js仍然是可以使用的）
+var Days;
+(function (Days) {
+    Days[Days["Sun"] = 7] = "Sun";
+    Days[Days["Mon"] = 8] = "Mon";
+    Days[Days["Tue"] = 9] = "Tue";
+    Days[Days["Wed"] = 10] = "Wed";
+    Days[Days["Thu"] = 11] = "Thu";
+    Days[Days["Fri"] = 12] = "Fri";
+    Days[Days["Sat"] = 'S'] = "Sat";
+})(Days || (Days = {}));
+console.log(Days);
+console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
--- practice/advanced/enum.ts
+++ practice/advanced/enum.ts
@@ -15,6 +15,7 @@ console.log(Weekly['Wen']) // 3
 console.log(Weekly['Sat']) // 6
 
 console.log(Weekly) // {"0":"Sun","1":"Mon","2":"Tue","3":"Wen","4":"Thu","5":"Fri","6":"Sat","Sun":0,"Mon":1,"Tue":2,"Wen":3,"Thu":4,"Fri":5,"Sat":6}
+console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
 
 // 2. 手动赋值
 enum Color { Red = 3, Green, Blue }
@@ -29,8 +30,12 @@ console.log(Color[4]) // Green
 console.log(Color[5]) // Blue
 
 console.log(Color)    // { '3': 'Red', '4': 'Green', '5': 'Blue', Red: 3, Green: 4, Blue: 5 }
+console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
 
-// 3. 只可以被覆盖
+// 3. 只可以被覆盖，但是最好不要出现覆盖的情况
 
 // 4. 手动赋值的 枚举项 可以不是数字，此时需要使用类型断言来让 tsc 无视类型检查（编译出的js仍然是可以使用的）
-enum Grade {  }
+enum Days { Sun = 7, Mon, Tue, Wed, Thu, Fri, Sat = <any>'S' }
+console.log(Days)
+
+console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
